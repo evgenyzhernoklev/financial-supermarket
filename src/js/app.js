b5c6ea63071f8fc320dec169f3812a9a2bb1b9eb
evgenyzhernoklev <oldschoolsnz@gmail.com>
2016-09-06 15:19:43 +0300

    slider added

--- src/js/app.js
+++ src/js/app.js
@@ -2,6 +2,7 @@
  * Third party
  */
 //= ../../node_modules/jquery/dist/jquery.min.js
+//= ../../node_modules/slick-carousel/slick/slick.min.js
 
 
 
--- src/js/common.js
+++ src/js/common.js
@@ -2,9 +2,11 @@ $(document).ready(function() {
     var $window = $(window),
         $body = $('body');
         
-    // $('.slider').slick({
-    //     infinite: true,
-    //     slidesToShow: 1,
-    //     slidesToScroll: 1
-    // });
+    $('.slider').slick({
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        draggable: false,
+        vertical: true
+    });
 });
