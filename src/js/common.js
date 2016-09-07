1fb9696a4507b90b82c326c0e431d5910d0991e9
evgenyzhernoklev <oldschoolsnz@gmail.com>
2016-09-07 13:22:30 +0300

    tabs block made

--- src/js/common.js
+++ src/js/common.js
@@ -1,7 +1,34 @@
 $(document).ready(function() {
     var $window = $(window),
         $body = $('body');
+    
+    
+    
+    
+    
+    //tabs
+    var $tabLinks = $('.tabsLinks_link'),
+        $tabsBlocks = $('.tabsContent');
+    
+    $tabLinks.on('click', function(e) {
+        e.preventDefault();
         
+        if ($(this).hasClass('is-active')) {
+            return false;
+        }
+        
+        var target = $(this).attr('href'),
+            $target = $(target);
+        
+        $tabLinks.removeClass('is-active');
+        $tabsBlocks.hide().removeClass('is-active');
+        $(this).addClass('is-active');
+        $target.fadeIn(300).addClass('is-active');
+    });
+    
+    
+    
+    //slider in header
     $('.slider').slick({
         infinite: true,
         slidesToShow: 1,
