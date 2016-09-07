c52ef246a1666934e1e88b27cdc368b0957669e5
evgenyzhernoklev <oldschoolsnz@gmail.com>
2016-09-07 15:27:42 +0300

    scrolling menu in sidebar

--- src/js/common.js
+++ src/js/common.js
@@ -1,6 +1,85 @@
 $(document).ready(function() {
     var $window = $(window),
-        $body = $('body');
+        $body = $('body'),
+        windowScrollTop = $window.scrollTop();
+    
+    
+    
+    //sidebar menu position
+    var $sidebarMenu = $('.menu__sidebar');
+        
+    if ($sidebarMenu.length) {
+        var sidebarMenuPosition = $sidebarMenu.offset().top,
+            sidebarMenuHeight = $sidebarMenu.height();
+        
+        function checkMenuPosition() {
+            var endPosition = $('.position-stop').offset().top - sidebarMenuHeight - 70;
+            windowScrollTop = $window.scrollTop();
+            
+            if (sidebarMenuPosition < windowScrollTop) {
+                $sidebarMenu.addClass('is-fixed');
+            } else {
+                $sidebarMenu.removeClass('is-fixed');
+            }
+            
+            if (windowScrollTop >= endPosition) {
+                $sidebarMenu.addClass('is-fixed-bottom');
+                $sidebarMenu.css('top', endPosition);
+            } else {
+                $sidebarMenu.removeClass('is-fixed-bottom');
+                $sidebarMenu.css('top', '0px');
+            }
+        }
+        
+        checkMenuPosition();
+        
+        $window.on('scroll', function() {
+            checkMenuPosition();
+        });
+    }
+    
+    
+    
+    //sidebar catalog
+    var $catalogLinks = $('.menu__sidebar_link'),
+        checkingElementsYPositions = [];
+    
+    if ($catalogLinks.length) {
+        $catalogLinks.each(function(index, element) {
+            var targetBlock = $(element).attr('href'),
+                $targetBlock = $(targetBlock),
+                startPosition = $targetBlock.offset().top - 21,
+                stopPosition = startPosition + $targetBlock.height(),
+                $targetBlockPosition = [startPosition, stopPosition];
+        
+            checkingElementsYPositions.push($targetBlockPosition);
+        });
+        
+        checkPosition(checkingElementsYPositions);
+        
+        $window.on('scroll', function() {
+            windowScrollTop = $window.scrollTop();
+            checkPosition(checkingElementsYPositions);
+        });
+    }
+    
+    $catalogLinks.on('click', function(e) {
+        e.preventDefault();
+        var targetBlock = $(this).attr('href'),
+            scrollTarget = $(targetBlock).offset().top - 20;
+        
+        $("html, body").stop().animate({scrollTop: scrollTarget}, '700', 'linear');
+    });
+    
+    function checkPosition(positions) {
+        for (var i = 0; i < positions.length; i++) {
+            if ((windowScrollTop > positions[i][0]) && (windowScrollTop < positions[i][1])) {
+                $catalogLinks.eq(i).addClass('is-active');
+            } else {
+                $catalogLinks.eq(i).removeClass('is-active');
+            }
+        }
+    }
     
     
     
